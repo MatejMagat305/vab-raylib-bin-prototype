917857cf02af79bcd014ee4d63bc3edf5d81e97c
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2025-05-15 12:46:59 +0200

    Create hello.v

---
+++ v/examples/hello.v
@@ -0,0 +1,23 @@
+import raylib as rl
+
+const screen_width = 800
+const screen_height = 600
+const white = rl.Color{255, 255, 255, 255}
+const blue = rl.Color{0, 0, 200, 255}
+
+@[export: 'android_run']
+fn run(){
+	rl.init_window(screen_width, screen_height, 'Example of using V and Raylib together')
+	rl.set_target_fps(60)
+	for !rl.window_should_close() {
+		rl.begin_drawing()
+		rl.clear_background(white)
+		rl.draw_text('Hello from V and Raylib.', 160, 270, 40, blue)
+		rl.end_drawing()
+	}
+	rl.close_window()
+}
+
+fn main(){
+	run()
+}
